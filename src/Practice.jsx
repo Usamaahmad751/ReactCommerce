dd12e1f574bd780734a36947acd22475314d5301
Usama ahmad <Usamaahmad751@gmail.com>
2020-03-04 23:48:47 +0500

    Update Practice.jsx

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -2,11 +2,7 @@ import React, { Component } from 'react';
 class Practice extends Component {
     state = {  }
     render() {
-        for (let i=24; i>0; i--) {
-            setTimeout( function timer(){
-             document.getElementById("timer").innerHTML = i;
-            }, i*3000 );
-        }
+      
         return ( <div id="timer">2060</div> );
     }
 }
