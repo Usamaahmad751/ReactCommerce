c4cabe1bdc7df58fd47a790c52c8ba965838ab66
Usama ahmad <Usamaahmad751@gmail.com>
2020-03-21 23:51:48 +0500

    Delete Practice.jsx

--- src/Practice.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-class Practice extends Component {
-    render() {
-      
-        return ( <div id="timer">2060</div> );
-    }
-}
- 
-export default Practice;
