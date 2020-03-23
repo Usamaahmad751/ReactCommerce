67f37bc1dee2dac3f1fa0ea3b35ff0dc84aa153e
Usama ahmad <Usamaahmad751@gmail.com>
2020-03-23 23:40:26 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,8 +11,4 @@ import Practice from './Practice';
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
