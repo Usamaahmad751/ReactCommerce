86cdf9448378105c3298ab1214bc2a9c0a3eadae
Usama ahmad <Usamaahmad751@gmail.com>
2020-03-23 23:39:48 +0500

    Update productDb.js

--- src/productDb.js
+++ src/productDb.js
@@ -2,10 +2,8 @@ import React, { Component } from 'react';
 class productDb extends Component {
     
     render() { 
-       
         return ( this.products );
     }
 }
- 
 export default productDb;
 
